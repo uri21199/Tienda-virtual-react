8005f36ae308450796f9211790e14875780baf3b
Lautaro Bustos <uri21199@gmail.com>
2021-12-22 17:08:09 -0300

    Mail de contacto

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import emailjs from 'emailjs-com'
+import './ContactForm.scss'
 
 const ContactForm = () => {
 
     const sendEmail = (e) => {
         e.preventDefault()
 
-        emailjs.sendForm('service_976qi77', 'template_5rj5rzc', e.target, 'user_0U6P75303pRyXOtIpAlWk')
+        emailjs.sendForm('service_976qi77', 'template_83y6spx', e.target, 'user_0U6P75303pRyXOtIpAlWk')
             .then((result) => {
                 console.log(result);
             })
@@ -19,16 +20,38 @@ const ContactForm = () => {
 
     return (
         <div>
-            <form onSubmit={sendEmail}>
-                <label>Nombre</label>
-                <input type="text" name='name'/>
-                <label>Email</label>
-                <input type="email" name='email'/>
-                <label>Mensaje</label>
-                <textarea name='message' rows="4"/>
-                <input type="submit" value="Enviar"/>
+            <h4 className='mx-3 mt-5'>Formulario</h4>
+            <p className='mx-3 mt-3 mb-5'>Si necesitás contactarte con nosotros completá el formulario, nos pondremos en contacto lo antes posible.</p>
+            {/* formulario de contacto */}
+            <form onSubmit={sendEmail} className='py-3'>
+                <div className="formContact">
+                <div className='m-3'>
+                    <label className='my-1'>Nombre y apellido</label>
+                    <input className='formInput' type="text" name="user_name" id="user_name"/>
+                </div>
+                <div className='m-3'>
+                    <label className='my-1'>Email</label>
+                    <input className='formInput' type="email" name="user_email" id="user_email"/>
+                </div>
+                <div className='m-3'>
+                    <label className='my-1'>Teléfono</label>
+                    <input className='formInput' type="number" name="user_tel" id="user_tel"/>
+                </div>
+                <div className='m-3'>
+                    <label className='my-1'>Localidad</label>
+                    <input className='formInput' type="text" name="user_city" id="user_city"/>
+                </div>
+                <div className='m-3'>
+                    <label className='my-1'>Asunto</label>
+                    <input className='formInput' type="text" name="user_subject" id="user_subject"/>
+                </div>
+                </div>
+                <div className='textareaInput'>
+                    <label className='my-1'>Mensaje</label>
+                    <textarea className='formInput' name="user_message" id="user_message" rows="5"></textarea>
+                </div>
+                <input type="submit" value="Enviar" className='inputSubmit'/>
             </form>
-
         </div>
     )
 }
